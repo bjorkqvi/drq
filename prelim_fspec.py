8ebb9d8bef78b8aedb4a7273f8073fb5739253ec
Jan-Victor Björkqvist <janvb@met.no>
2024-02-05 15:08:35 +0100

    added basic plotting scripts

---
+++ prelim_fspec.py
@@ -0,0 +1,70 @@
+import xarray as xr
+import matplotlib.pyplot as plt
+import cmocean.cm
+import numpy as np
+import scipy as sp
+import pandas as pd
+
+
+def interp_nan(ts: xr.DataArray) -> tuple[np.ndarray, np.ndarray]:
+    """Interpolates NaN values from the yanked eta-timeseries and determined dt [s]"""
+    dt = int(np.mean(np.diff(ts.time))) / 1_000_000_000
+    z = pd.Series(ts.eta.values).interpolate().tolist()
+    return z, dt
+
+
+def calc_fspec(z: np.ndarray, fs: float) -> tuple[np.ndarray, np.ndarray, float]:
+    f, E = sp.signal.welch(z, fs=fs, nperseg=len(z) / 8)
+    df = np.mean(np.diff(f))
+    return f, E, df
+
+
+def plot_spec(ax, f, E, loglog: bool = False, label: str = "_"):
+    if loglog:
+        ax.loglog(f, E)
+    else:
+        ax.plot(f, E, label=label)
+        ax.set_xlim([0, 0.5])
+    ax.set_xlabel("f (Hz)")
+    ax.set_ylabel("E(f) (m^2/Hz)")
+
+    return ax
+
+
+def plot_saturation_spec(ax, f, E, power: float = 5):
+    w = f * 2 * np.pi
+    S = E / 2 / np.pi
+    ax.loglog(w, S * w**power, label="_")
+    ax.set_xlabel("w (rad/s)")
+    ax.set_ylabel(f"S(w)*w^{power:.1f}")
+
+    return ax
+
+
+ds = xr.open_dataset("sv_data/ekofisk/xygrid_50cm_20191209_1200_plane_sub.nc")
+fig, ax = plt.subplots(2, 2)
+for x in range(49, 52):
+    for y in range(49, 52):
+        ts = ds.isel(x=x, y=y)
+
+        z, dt = interp_nan(ts)
+        f, E, df = calc_fspec(z, 1 / dt)
+
+        ax[0, 0] = plot_spec(
+            ax[0, 0],
+            f,
+            E,
+            label=f"x {ts.x.values}, y {ts.y.values} / Hs_raw/m0 = {4*np.sqrt(np.var(z)):.2f}/{4*np.sqrt(np.sum(E)*df):.2f}",
+        )
+        ax[0, 1] = plot_spec(ax[0, 1], f, E, loglog=True)
+
+        ax[1, 0] = plot_saturation_spec(ax[1, 0], f, E, power=4)
+        ax[1, 1] = plot_saturation_spec(ax[1, 1], f, E, power=5)
+
+
+ax[1, 1].loglog(
+    2 * np.pi * f, np.ones(len(f)) * 9.81**2 * 10**-2, "k--", label="g^2*10^-2"
+)
+ax[1, 1].legend()
+ax[0, 0].legend()
+fig.show()
---
+++ prelim_plot_xyz.py
@@ -0,0 +1,12 @@
+import xarray as xr
+import matplotlib.pyplot as plt
+import cmocean.cm
+import numpy as np
+
+ds = xr.open_dataset("sv_data/ekofisk/xygrid_50cm_20191209_1200_plane_sub.nc")
+ds["ygrid"] = np.abs(ds.ygrid)
+ds["y"] = np.abs(ds.y)
+
+fig, ax = plt.subplots(1)
+ax.pcolormesh(ds.xgrid, ds.ygrid, ds.isel(time=4869).eta, cmap=cmocean.cm.diff)
+fig.show()
