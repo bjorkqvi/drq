448d74fbe41fe5af30b71f1c8970c1716c30052f
Jan-Victor Björkqvist <janvb@met.no>
2024-02-16 16:38:24 +0100

    fixed ktheta interpolation

--- drq/spectra/spec2d.py
+++ drq/spectra/spec2d.py
@@ -19,6 +19,7 @@ import pandas as pd
 
 from scipy.interpolate import griddata
 from .attributes import SpecAttributes
+import matplotlib.pyplot as plt
 
 
 @add_datavar(name="spec")
@@ -39,30 +40,67 @@ class FkTheta(PointSkeleton, SpecAttributes):
     @classmethod
     def from_fkxy(cls, fkxy: Fkxy) -> FkTheta:
         dk = 2 * np.pi / 80
-
         kx, ky = np.meshgrid(fkxy.kx(), fkxy.ky())
         kxy = (kx**2 + ky**2) ** 0.5
-        theta = np.arctan2(kx, ky)
-        mask = theta < 0
-        theta[mask] = theta[mask] + 2 * np.pi
-
-        theta_vec = np.deg2rad(np.arange(0, 360, 5))
-        # k_vec = np.arange(dk, np.max(fkxy.kx()), dk)
+        spec_data = fkxy.spec()
         k_vec = np.arange(0.01, 7, dk)
-        theta_grid, k_grid = np.meshgrid(
+
+        # Interpolate using 0 to 2*pi so we are continuous around 180
+        dD = 5
+        # Captures energy better to use a high dtheta in interpolation
+        refine_factor = 50
+        theta_south = np.arctan2(kx, ky)
+        theta_vec = np.deg2rad(np.arange(0, 360, dD / refine_factor))
+        theta_grid_south, k_grid = np.meshgrid(
             theta_vec,
             k_vec,
         )
-        spec_data = fkxy.spec()
-        spec = griddata(
-            (theta.ravel(), kxy.ravel()),
+        spec_south = griddata(
+            (theta_south.ravel(), kxy.ravel()),
             spec_data.ravel(),
-            (theta_grid, k_grid),
+            (theta_grid_south, k_grid),
             method="linear",
         )
+
+        # Interpolate using -pi to pi so we are continuous around 0
+        theta_north = np.arctan2(kx, ky)
+        mask = theta_north > np.pi
+        theta_north[mask] = theta_north[mask] - 2 * np.pi
+
+        theta_grid_north = np.zeros(theta_grid_south.shape)
+        mask = theta_grid_south > np.pi
+        theta_grid_north[np.logical_not(mask)] = theta_grid_south[np.logical_not(mask)]
+        theta_grid_north[mask] = theta_grid_south[mask] - 2 * np.pi
+        spec_north = griddata(
+            (theta_north.ravel(), kxy.ravel()),
+            spec_data.ravel(),
+            (theta_grid_north, k_grid),
+            method="linear",
+        )
+
+        # Combine north and south spec
+        spec = spec_north
+        ind0, ind1 = (
+            len(theta_vec) // 2 - len(theta_vec) // 4,
+            len(theta_vec) // 2 + len(theta_vec) // 4,
+        )
+        spec[:, ind0:ind1] = spec_south[:, ind0:ind1]
+
         spec[np.isnan(spec)] = 0
-        fktheta = cls(lon=fkxy.lon(), lat=fkxy.lat(), theta=theta_vec, k=k_vec)
-        fktheta.set_spec(spec, allow_reshape=True)
+
+        # Bin average down to dD (e.g. 5) degrees
+        val_roll = np.matlib.repmat(spec.T, 3, 1).T
+        theta_vec_mean = theta_vec[::refine_factor]
+        spec_mean = np.zeros((spec.shape[0], len(theta_vec_mean)))
+        for i, n in enumerate(
+            range(refine_factor, refine_factor + spec.shape[1], refine_factor)
+        ):
+            spec_mean[:, i] = np.mean(
+                val_roll[:, n - refine_factor // 2 : n + refine_factor // 2], 1
+            )
+
+        fktheta = cls(lon=fkxy.lon(), lat=fkxy.lat(), theta=theta_vec_mean, k=k_vec)
+        fktheta.set_spec(spec_mean, allow_reshape=True)
         return fktheta
 
     def plot(self):
@@ -79,6 +117,8 @@ class FkTheta(PointSkeleton, SpecAttributes):
             vmax=self.spec().max(),
             alpha=0.8,
         )
+        ax.set_xlabel("k [rad/m]")
+        ax.set_ylabel("theta [rad]")
 
         # ax.pcolormesh(dirs, k, np.log(self.spec(squeeze=True)))
         fig.show()
@@ -142,7 +182,7 @@ class Fkxy(PointSkeleton, SpecAttributes):
         spec_ds = f3d.ds().integrate(coord="freq")
         return cls.from_ds(spec_ds)
 
-    def m(self, moment: float) -> float:
+    def m(self, moment: float, method: str = "integrate") -> float:
         return (
             self.spec(data_array=True)
             .integrate(coord="kx")
@@ -154,6 +194,9 @@ class Fkxy(PointSkeleton, SpecAttributes):
         fig, ax = plt.subplots()
         kx, ky = np.meshgrid(self.kx(), self.ky())
         ax.pcolormesh(kx, ky, np.log(self.spec(squeeze=True)))
+        ax.set_xlabel("kx [rad/m]")
+        ax.set_ylabel("ky [rad/m]")
+
         fig.show()
 
 
